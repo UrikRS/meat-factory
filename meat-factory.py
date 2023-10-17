ed208272737482e48e497619cd3e78138dad83d4
Urik Rocksalt <610521224@gms.ndhu.edu.tw>
2023-10-17 23:51:02 +0800

    first commit

---
+++ meat-factory.py
@@ -0,0 +1,55 @@
+from time import localtime, strftime, sleep
+from threading import Thread, Lock
+import random
+
+
+lock = Lock()
+
+
+def now():
+    return strftime("%Y-%m-%d %H:%M:%S", localtime())
+
+
+def take_meat(worker):
+    with lock:
+        global meats
+        if meats:
+            meat = random.choice(meats)
+            meats.remove(meat)
+            print(f'{worker} 在 {now()} 取得{meat}')
+            return meat
+        else:
+            return None
+
+
+def meat_processing(worker, meat):
+    while meat:
+        match meat:
+            case '牛肉':
+                sleep(1)
+                print(f'{worker} 在 {now()} 處理完牛肉')
+            case '豬肉':
+                sleep(2)
+                print(f'{worker} 在 {now()} 處理完豬肉')
+            case '雞肉':
+                sleep(3)
+                print(f'{worker} 在 {now()} 處理完雞肉')
+        meat = take_meat(worker)
+
+
+class Worker:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.work = Thread(
+            target=meat_processing,
+            args=(self.name, take_meat(self.name)),
+        )
+
+
+if __name__ == '__main__':
+    meats = ['牛肉'] * 10 + ['豬肉'] * 7 + ['雞肉'] * 5
+    worker_names = ['A', 'B', 'C', 'D', 'E']
+    workers = [Worker(name) for name in worker_names]
+
+    for worker in workers:
+        worker.work.start()
